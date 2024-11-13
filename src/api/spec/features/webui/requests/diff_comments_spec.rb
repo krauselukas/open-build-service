ed7ed16e6c0cd9181f5c445bc94cb02b4cf5fc47
Lukas Krause <lkrause@suse.de>
2024-11-13 23:11:41 +0100

    Add feature test to ensure outdated inline diff comment when target alters

--- src/api/spec/features/webui/requests/diff_comments_spec.rb
+++ src/api/spec/features/webui/requests/diff_comments_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe 'Comments with diff', :js, :vcr do
   let(:admin) { create(:admin_user, login: 'Admin') }
   let(:target_project) { create(:project, name: 'target_project') }
   let(:source_project) { create(:project, :as_submission_source, name: 'source_project') }
-  let(:target_package) { create(:package, name: 'target_package', project: target_project) }
+  let(:target_package) do
+    create(:package_with_files, name: 'target_package', project: target_project)
+  end
+
   let(:source_package) do
     create(:package_with_files,
            name: 'package_a',
@@ -102,6 +105,44 @@ RSpec.describe 'Comments with diff', :js, :vcr do
 
       it 'the changes from the altered source package file are displayed in the diff' do
         expect(page).to have_text('The content of the changes file has completly changed!')
+      end
+
+      it 'does not display the outdated comment in the changes tab' do
+        expect(page).to have_no_text(comment.body)
+      end
+    end
+
+    context 'conversation tab' do
+      before do
+        visit request_show_path(bs_request)
+      end
+
+      it 'keeps showing the comment in the conversation with a hint that it is outdated' do
+        expect(find_by_id("comment-#{comment.id}-bubble")).to have_text(comment.body)
+        expect(page).to have_css('span.badge.text-bg-warning', text: 'Outdated')
+      end
+    end
+  end
+
+  describe 'target package gets altered after inline diff comment was created' do
+    let!(:comment) do
+      create(:comment, commentable: bs_request.bs_request_actions.first, diff_file_index: 0, diff_line_number: 1, user: admin, source_rev: bs_request.bs_request_actions.first.source_rev,
+                       target_rev: target_package.dir_hash['srcmd5'])
+    end
+
+    before do
+      Flipper.enable(:request_show_redesign, admin)
+      login admin
+      target_package.save_file(file: 'The content of the changes file has completly changed!', filename: "#{target_package.name}.changes", comment: 'No reason, this is just a test...')
+    end
+
+    context 'changes tab' do
+      before do
+        visit request_changes_path(bs_request)
+      end
+
+      it 'the changes from the altered target package file are displayed in the diff' do
+        expect(page).to have_text("#{target_package.name}.changes")
       end
 
       it 'does not display the outdated comment in the changes tab' do
